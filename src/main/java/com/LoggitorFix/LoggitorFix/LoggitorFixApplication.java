e86fc9c28eb112410c51d71703156ca44d17ce9e
Fady Kittan <fady.kittan@gmail.com>
2019-01-15 23:44:38 +0200

    my first commit

---
+++ src/main/java/com/LoggitorFix/LoggitorFix/LoggitorFixApplication.java
@@ -0,0 +1,14 @@
+package com.LoggitorFix.LoggitorFix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoggitorFixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoggitorFixApplication.class, args);
+	}
+
+}
+
