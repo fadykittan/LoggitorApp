c9d0ab443c04c577102afd4716b3d195ab18f664
Fady Kittan <fady.kittan@gmail.com>
2019-01-16 18:48:48 +0200

    Changing the test package name

--- src/test/java/com/LoggitorApp/LoggitorApp/LoggitorAppApplicationTests.java
+++ src/test/java/com/LoggitorApp/LoggitorApp/LoggitorAppApplicationTests.java
@@ -1,4 +1,4 @@
-package com.LoggitorFix.LoggitorFix;
+package com.LoggitorApp.LoggitorApp;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class LoggitorFixApplicationTests {
+public class LoggitorAppApplicationTests {
 
 	@Test
 	public void contextLoads() {
